a3010a05be705d47e4a1003a67cd32dc0fa3f2c6
Carsten Bachert <86466914+caheba@users.noreply.github.com>
2025-10-19 16:52:09 +0200

    Create setup_dci_env.sh
    initial

---
+++ setup_dci_env.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+ 
+git clone http://github.com/caheba/dcinfo.git
+python3 -m venv dcinfo
+cd dcinfo
+source bin/activate
+python3 -m pip install django
+cd dcinfo
+python3 manage.pymakemigrations
+python3 manage.py migrate
+python3 manage.py createsuperuser
+
+
+echo "zum Starten:"
+echo "cd dcinfo"
+echo "source bin/activate"
+echo "python3 manage.py runserver
