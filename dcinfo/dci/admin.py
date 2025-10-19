8604f435a624ddf75dd8d1402687507f4318fe58
Carsten Bachert <86466914+caheba@users.noreply.github.com>
2025-10-19 19:25:05 +0200

    Add files via upload
    Variablen angepasst

--- dcinfo/dci/admin.py
+++ dcinfo/dci/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Change 
+from .models import VServer
+
 # Register your models here.
+
+admin.site.register(Change)
+admin.site.register(VServer)
--- dcinfo/dci/models.py
+++ dcinfo/dci/models.py
@@ -3,7 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Change(models.Model):
-    number = models.CharField(max_length=10)
-    title = models.CharField(max_length=80)
+    chg_number = models.CharField(max_length=10)
+    chg_title = models.CharField(max_length=80,unique=True)
 
+    def __str__(self):
+        return f'{self.chg_number} - {self.chg_title}'
+
+class VServer(models.Model):
+    vs_shortname = models.CharField(max_length=10,unique=True)
+    vs_type = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f'{self.vs_shortname}'
 
