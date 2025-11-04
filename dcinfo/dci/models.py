7a573df3d9e0452d96322ac27464769d937a85fb
Carsten Bachert <carsten@bachert.info>
2025-11-04 07:52:39 +0100

    changes gestartet

--- dcinfo/dci/models.py
+++ dcinfo/dci/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Change(models.Model):
     chg_number = models.CharField(max_length=10)
     chg_title = models.CharField(max_length=80,unique=True)
+    chg_text = models.TextField(default="")
 
     def __str__(self):
         return f'{self.chg_number} - {self.chg_title}'
--- dcinfo/dci/views.py
+++ dcinfo/dci/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
+from .models import Change
 # Create your views here.
+
+#def index(request):
+#    chg_list = Change.objects.order_by("-chg_number")[:3]
+#    #chg_list = ["Carsten","Hugo","Martin"]
+#    output = ", ".join([q.chg_title for q in chg_list])
+#    return HttpResponse(output)
+#    #return HttpResponse('Hier die Ausgabe von DCI')
+
+
+def index(request):
+    chg_list = Change.objects.order_by("-chg_number")[:5]
+    template = loader.get_template("dci/index.html")
+    context = {"chg_list": chg_list}
+    return HttpResponse(template.render(context, request))
